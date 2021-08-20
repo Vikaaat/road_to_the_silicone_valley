9717bf405cd1c14abc27743e5bcadab4942955c3
Vika <47684687+Vikaaat@users.noreply.github.com>
2021-08-21 01:08:23 +0400

    header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import { Button } from '@design-system-rt/rtk-ui-kit';
+import { Header} from '@design-system-rt/rtk-ui-kit';
+import { Footer} from '@design-system-rt/rtk-ui-kit';
+
 function App() {
   return (
     <div className="App">
-      <Button onClick={() => {}}>Button</Button>
+      <Header></Header>
+      <Footer children="Продолжая пользование сайтом, вы соглашаетесь на обработку файлов Cookies и других пользовательских данных, в соответствии с политикой конфиденциальности"></Footer>
+
     </div>
   );
 }
-
 export default App;
