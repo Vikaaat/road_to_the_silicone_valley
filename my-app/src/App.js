741821f46f4ce1877734a63b065e38fc6c7053e4
Dmitriy_Rybkin <prib-181_993259@volsu.ru>
2021-08-21 08:01:33 +0300

    STYLE CHANGE

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Header, Footer, Button, Checkbox, TextArea, Box, Select} from '@design-system-rt/rtk-ui-kit';
+import { Header, Footer, Button, InputNumberStepper} from '@design-system-rt/rtk-ui-kit';
 import React from 'react';
 const h=<Header/>
 class App extends React.Component {
@@ -12,11 +12,13 @@ class App extends React.Component {
     children: "test",
   }
   onClickHeader = () => {
+    document.querySelector('.inspector').style.opacity=0
     this.setState(({ countHeader }) => ({
       countHeader: countHeader + 1,
     }));
   }
   onClickButton = () => {
+    document.querySelector('.inspector').style.opacity=1
     this.setState(({ countButton }) => ({
       countButton: countButton + 1,
     }));
@@ -40,18 +42,25 @@ class App extends React.Component {
     
     let ofX = document.getElementById('main-root').offsetLeft
     let ofY = document.getElementById('main-root').offsetTop
-    e.target.style.top=(e.pageY - ofY) + 'px'
-    e.target.style.left=(e.pageX - ofX) + 'px'
+    let ofW = document.getElementById('main-root').offsetWidth
+    let ofH = document.getElementById('main-root').offsetHeight
+    let testY = e.pageY - ofY
+    let testX = e.pageX - ofX
+    if(testY > ofH || testY < 0 || testX > ofW || testX <0){
+      e.target.style.top='0px'
+      e.target.style.left='0px'
+    }
+    else{
+      e.target.style.top=(testY) + 'px'
+      e.target.style.left=(testX) + 'px'
+    }
+    
   }
   gretProp = (e) => {
-      console.log(Button)
-    this.setState(({ color }) => ({
       
+    this.setState(({ color }) => ({
       color: e.target.value,
     }));
-  }
-  getTest = (e) => {
-    console.log(e)
   }
   render() {
     let currentColor = this.state.color
@@ -60,20 +69,22 @@ class App extends React.Component {
         <Header></Header>
         <main>
           <div class="list-object">
+            <h1 class="title">Компоненты</h1>
             <div class="object-item" onClick={this.onClickHeader}>
               <Header></Header>
             </div>
 
             <div class="object-item" onClick={this.onClickButton}>
-              <Button></Button>
+              <Button color={currentColor}>{this.state.children}</Button>
             </div>
            
           </div>
           <div id="main-root">
           {[...Array(this.state.countHeader)].map(() => <Header draggable onDragStart={this.onDraggble} onDrag={this.moveDraggble} onDragEnd={this.EndDraggble}/>)}
-          {[...Array(this.state.countButton)].map(() => <Button onClick={this.getTest(this)} color={currentColor} draggable onDragStart={this.onDraggble} onDrag={this.moveDraggble} onDragEnd={this.EndDraggble}>{this.state.children}</Button>)}
+          {[...Array(this.state.countButton)].map(() => <Button color={currentColor} draggable onDragStart={this.onDraggble} onDrag={this.moveDraggble} onDragEnd={this.EndDraggble}>{this.state.children}</Button>)}
           </div>
           <div class="inspector">
+            <h1 class="title">Стили</h1>
             <select onChange={this.gretProp} >
               <option value="primary1">Фиолетовый</option>
               <option value="primary2">Оранжевый</option>
