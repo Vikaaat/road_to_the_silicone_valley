f2ff16be7f64d711f3233db786ff9b30093b16e3
Dmitriy_Rybkin <prib-181_993259@volsu.ru>
2021-08-22 08:03:24 +0300

    d

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,33 +17,24 @@ import Head from './components/Head.js';
 import Nav from './components/Nav.js';
 import Foot from './components/Foot.js';
 import Control from './components/Control.js';
-
-const App = () => <div className="App">
-<Header></Header>
-    <Router>
-
-                    <Switch>
-                        <Route path="/template_web/" component={Template_web} exact>
-                        </Route>
-                        <Route path="/template_mob/" component={Template_mob} exact>
-                        </Route>
-                        <Route path="/" component={Home} exact>
-                        </Route>
-
-                    </Switch>
-                </Router>
-                
-                <Footer children="Продолжая пользование сайтом, вы соглашаетесь на обработку файлов Cookies и других пользовательских данных, в соответствии с политикой конфиденциальности"></Footer>
-    </div>
-  
-
-
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Head />
         <Nav />
+        <Router>
+
+          <Switch>
+              <Route path="/template_web/" component={Template_web} exact>
+              </Route>
+              <Route path="/template_mob/" component={Template_mob} exact>
+              </Route>
+              <Route path="/" component={Home} exact>
+              </Route>
+
+          </Switch>
+          </Router>
         <main>
           <Control />
         </main>
