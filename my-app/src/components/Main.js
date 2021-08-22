ecf9196ce77fd2cf90ccfe6bfe91ccd7f2529d90
Dmitriy_Rybkin <prib-181_993259@volsu.ru>
2021-08-22 05:01:54 +0300

    more

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -2,6 +2,23 @@ import '../App.css';
 import React from 'react';
 import { Header, Footer, Button, InputNumberStepper} from '@design-system-rt/rtk-ui-kit';
 class Main extends React.Component{
+    onDragEnd(e)
+    {
+        let ofX = document.getElementById('main-root').offsetLeft
+        let ofY = document.getElementById('main-root').offsetTop
+        let ofW = document.getElementById('main-root').offsetWidth
+        let ofH = document.getElementById('main-root').offsetHeight
+        let testY = e.pageY - ofY
+        let testX = e.pageX - ofX
+        if(testY > ofH || testY < 0 || testX > ofW || testX < 0){
+            e.target.style.top='0px'
+            e.target.style.left='0px'
+        }
+        else{
+            e.target.style.top=(testY) + 'px'
+            e.target.style.left=(testX) + 'px'
+        }
+    }
     render(){
         var prop = this.props.draw_elements;
         if (prop.length > 0)
@@ -10,8 +27,8 @@ class Main extends React.Component{
             var but = prop.filter(item => item === "Button");
             return(
                 <div id="main-root">
-                    {head.map(head => <Header />)}
-                    {but.map(but => <Button />)}
+                    {head.map(head => <Header draggable onDragEnd = {this.onDragEnd}/>)}
+                    {but.map(but => <Button draggable onDragEnd = {this.onDragEnd}/>)}
                 </div>
             );
         }
