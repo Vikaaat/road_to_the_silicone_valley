94fd347a6359585370fb1b5d1f638c7c78d8e088
Dmitriy_Rybkin <prib-181_993259@volsu.ru>
2021-08-22 04:34:14 +0300

    more

--- my-app/src/components/Control.js
+++ my-app/src/components/Control.js
@@ -6,20 +6,16 @@ import Tools from './Tools.js';
 class Control extends React.Component{
     constructor(){
         super()
-        this.state = {count: 0, element: "", elements: [{id: -1, text: ""}]}
+        this.state = {element: "", elements: []}
     }
     updateData = (value) => {
         this.setState({element: value})
-        let todos = [...this.state.elements];
-        todos.push({id: this.state.count, text: value});
-        this.setState({elements: todos});
-        this.setState({count: this.state.count + 1})
+        this.setState({elements: [...this.state.elements, value]})
     }
     render(){
         return(
             <>
             <Tools updateData = {this.updateData} />
-            {this.state.elements[0].id}
             <Main draw_elements = {this.state.elements}/>
             <Inspector />
             </>
